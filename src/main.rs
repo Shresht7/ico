5916421a18f782bd94954a4092ea0b08c1c30e42
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-13 20:52:21 +0530

    Implement proof-of-concept
    Generate an ICO file from a PNG. It looks like the `image` crate has everything i need.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
+use image::codecs::ico::{IcoEncoder, IcoFrame};
+
 mod cli;
 
 fn main() {
     let cli = cli::parse();
-    println!(
-        "{} -> {} ({})",
-        cli.input.display(),
-        cli.output.display(),
-        cli.sizes
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<_>>()
-            .join(",")
-    )
+    if let Err(e) = run(&cli) {
+        eprintln!("Error: {e}");
+        std::process::exit(1);
+    }
+}
+
+fn run(args: &cli::Args) -> std::io::Result<()> {
+    if !args.input.extension().is_some_and(|e| e == "png") {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Unsupported,
+            "Unsupported extension",
+        ));
+    }
+
+    let img = image::open(&args.input).expect("failed to open image");
+
+    let img = img.resize(16, 16, image::imageops::FilterType::Lanczos3);
+    let rgba = img.to_rgba8();
+
+    let frame = IcoFrame::as_png(&rgba, 16, 16, image::ExtendedColorType::Rgba8)
+        .expect("failed to make ico frame");
+
+    let file = std::fs::File::create(&args.output).expect("failed to create output file");
+    IcoEncoder::new(file)
+        .encode_images(&[frame])
+        .expect("failed to encode to ico");
+
+    Ok(())
 }

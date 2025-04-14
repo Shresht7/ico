49a332ae489e5e602d246de5ee0880c184f5a9bc
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-14 15:06:31 +0530

    Refactor `generate_frames` to handle multiple sizes and return a vector of ICO frames

--- src/png.rs
+++ src/png.rs
@@ -3,12 +3,21 @@ use image::codecs::ico::IcoFrame;
 
 use crate::cli;
 
+/// Generates [`IcoFrame`]s from a PNG file
+/// This function takes a file path and a list of sizes, and generates ICO frames for each size.
 pub fn generate_frames(args: &cli::Args) -> anyhow::Result<Vec<IcoFrame>> {
     let img = image::open(&args.input).context("failed to open file")?;
-
-    let img = img.resize(16, 16, image::imageops::FilterType::Lanczos3);
-    let rgba = img.to_rgba8();
-    let frame = IcoFrame::as_png(&rgba, 16, 16, image::ExtendedColorType::Rgba8)
-        .context("failed to make ico frame")?;
-    Ok(vec![frame])
+    let frames: Vec<IcoFrame> = args
+        .sizes
+        .iter()
+        .map(|size| -> Result<IcoFrame<'_>, anyhow::Error> {
+            let img = img.resize(*size, *size, image::imageops::FilterType::Lanczos3);
+            let rgba = img.to_rgba8();
+            Ok(
+                IcoFrame::as_png(&rgba, *size, *size, image::ExtendedColorType::Rgba8)
+                    .context("failed to make ico frame")?,
+            )
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+    Ok(frames)
 }

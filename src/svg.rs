19f8bb6c74ac36cd1735b40a83d3ec74e980de56
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-14 14:45:25 +0530

    Extract logic to generate a single frame into a separate function

--- src/svg.rs
+++ src/svg.rs
@@ -3,36 +3,42 @@ use resvg::tiny_skia::{self, Pixmap};
 
 use crate::cli;
 
+/// Generates [`IcoFrame`]s from an SVG file
 pub fn generate_frames(args: &cli::Args) -> Result<Vec<IcoFrame>, Box<dyn std::error::Error>> {
     let file_contents = std::fs::read(&args.input)?;
-
     let tree = usvg::Tree::from_data(&file_contents, &usvg::Options::default())?;
+    let frame = generate_frame(&tree, 256)?;
+    Ok(vec![frame])
+}
 
-    let size = 256.0;
-    let size =
-        tiny_skia::Size::from_wh(size, size).expect("unsigned values should always be valid");
-
+/// Rasterizes the given [SVG tree][usvg::Tree] into an [`IcoFrame`] of given `size`
+fn generate_frame(
+    tree: &usvg::Tree,
+    size: usize,
+) -> Result<IcoFrame<'static>, Box<dyn std::error::Error>> {
+    // Scale the SVG to fit the given size
+    let size = tiny_skia::Size::from_wh(size as f32, size as f32)
+        .expect("unsigned values should always be valid");
     let scaled_size = tree.size().scale_to(size);
     let sx = scaled_size.width() / tree.size().width();
     let sy = scaled_size.height() / tree.size().height();
     let transform = tiny_skia::Transform::from_scale(sx, sy);
 
+    // Render the SVG to a pixmap of the specified size
     let pixmap_size = scaled_size.to_int_size();
     let mut pixmap =
         Pixmap::new(pixmap_size.width(), pixmap_size.height()).expect("failed to create pixmap");
-    resvg::render(&tree, transform, &mut pixmap.as_mut());
+    resvg::render(tree, transform, &mut pixmap.as_mut());
 
+    // Convert the pixmap to an image and create an IcoFrame from it
     let img = image::RgbaImage::from_raw(pixmap.width(), pixmap.height(), pixmap.data().to_vec())
         .ok_or("failed to convert pixmap to image")
         .unwrap();
 
-    let frame = IcoFrame::as_png(
+    Ok(IcoFrame::as_png(
         &img,
         pixmap_size.width(),
         pixmap_size.height(),
         image::ExtendedColorType::Rgba8,
-    )
-    .expect("failed to make ico frame");
-
-    Ok(vec![frame])
+    )?)
 }
